6541f0abee3e97083a4e3e7be4be23d11349b981
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-10-06 16:39:18 +0700

    Lab 5 seadanya

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,13 +1,7 @@
 import imp
 from django.urls import path
-from wishlist.views import show_wishlist
-from wishlist.views import show_xml
-from wishlist.views import show_json
-from wishlist.views import show_json_by_id
-from wishlist.views import register
-from wishlist.views import login_user
-from wishlist.views import logout_user
-
+from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, register, login_user, logout_user
+from wishlist.views import show_wishlist_ajax
 
 app_name = 'wishlist'
 
@@ -19,4 +13,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('wishlist/ajax', show_wishlist_ajax, name='show_wishlist_ajax'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -17,7 +17,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 @login_required(login_url='/wishlist/login/')
-
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
@@ -77,3 +76,13 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('wishlist:login'))
     response.delete_cookie('last_login')
     return response
+
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Nikolas Reva Bellie',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
